4bd23f3253023c0c3c6b13837a36a5a47b8918a3
Joseph Scott <joseph@josephscott.org>
2022-01-15 16:38:36 -0700

    Test for delayed flush_all

--- tests/FlushTest.php
+++ tests/FlushTest.php
@@ -5,3 +5,25 @@ test( 'flush_all', function() {
 	$result = MC::$mc->flush();
 	$this->assertEquals( true, $result );
 } );
+
+test( 'flush_all delay', function() {
+	$key = 'thing';
+	$value = 'abc';
+	$delay = 3;
+
+	$result = MC::$mc->add( $key, $value );
+	$this->assertEquals( true, $result );
+
+	$result = MC::$mc->get( $key );
+	$this->assertEquals( $value, $result );
+
+	$result = MC::$mc->flush( $delay );
+	$this->assertEquals( true, $result );
+
+	$result = MC::$mc->get( $key );
+	$this->assertEquals( $value, $result );
+
+	sleep( $delay + 1 );
+	$result = MC::$mc->get( $key );
+	$this->assertEquals( false, $result );
+} );
