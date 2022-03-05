eb781ad334aa34860484b2a5ff0b9cd5f45c8242
Joseph Scott <joseph@josephscott.org>
2022-03-04 21:59:08 -0700

    Start incr tests

---
+++ tests/incr-tests.php
@@ -0,0 +1,15 @@
+<?php
+declare( strict_types = 1 );
+
+test( 'incr', function() {
+	$key = 'thing';
+
+	$result = MC::$mc->add( $key, 1 );
+	$this->assertEquals( true, $result );
+
+	$result = MC::$mc->get( $key );
+	$this->assertEquals( 1, $result );
+
+	$result = MC::$mc->increment( $key, 1 );
+	$this->assertEquals( 2, $result );
+} );
