761b3097df73a2862cd9ba6a91bcd9592d8e8776
Joseph Scott <joseph@josephscott.org>
2022-04-15 15:06:00 -0600

    Add test for gets

---
+++ tests/gets-tests.php
@@ -0,0 +1,15 @@
+<?php
+declare( strict_types = 1 );
+
+test( 'gets', function() {
+	$key = 'thing';
+	$value = 'abc';
+
+	$result = MC::$mc->add( $key, $value );
+	$this->assertEquals( true, $result );
+
+	$result = MC::$mc->get( $key, null, Memcached::GET_EXTENDED );
+	expect( $result )->toBeArray();
+	expect( $result )->toHaveKeys( [ 'value', 'cas', 'flags' ] );
+	$this->assertEquals( $value, $result['value'] );
+} );
