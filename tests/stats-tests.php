4769be58d7f668ac6a96450dd9a513d60f0b7d5e
Joseph Scott <joseph@josephscott.org>
2022-04-20 15:25:08 -0600

    New test: stats uptime

--- tests/stats-tests.php
+++ tests/stats-tests.php
@@ -8,3 +8,15 @@ test( 'stats', function() {
 	expect( $result )->toBeArray();
 	$this->assertEquals( '127.0.0.1:11211', $first_key );
 } );
+
+test( 'stats uptime', function() {
+	$result = MC::$mc->getStats();
+
+	expect( $result['127.0.0.1:11211']['uptime'] )->toBeInt();
+
+	# Really this could probably be only greater than zero, as most
+	# tests are going to involve at least one second after the server starts.
+	# But to make this test more reliable I'm going with the technically
+	# more accurate greater than or equal.
+	expect( $result['127.0.0.1:11211']['uptime'] )->toBeGreaterThanOrEqual( 0 );
+} );
